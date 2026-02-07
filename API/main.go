bc5aa4ea625f5738cb2bb8e3372f287a75e99999
Pankaj-Gupta25 <pankaj9852442481@gamil.com>
2026-02-08 01:34:41 +0530

    we have connected the ui for the project to connect the backend with the frontend we have used the cors module in gofiber

--- API/main.go
+++ API/main.go
@@ -7,13 +7,20 @@ import (
 
 	routes "github.com/Pankaj-Gupta25/Email_shortener_golang_radis/Routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 )
 
 func setupRoutes(app *fiber.App) {
-	app.Get("/:url", routes.ResolveURL)
+
 	app.Post("/api/v1", routes.ShortenURL)
+		app.Static("/", "../ui", fiber.Static{
+		Index: "index.html",
+	})
+
+	app.Get("/:url", routes.ResolveURL)
+	
 }
 
 func main() {
@@ -23,6 +30,14 @@ func main() {
 		fmt.Println(err)
 	}
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	app.Use(logger.New())
+
 	app.Use(logger.New())
 
 	setupRoutes(app)
